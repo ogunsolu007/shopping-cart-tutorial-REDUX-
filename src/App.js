60e655f42e6db336c77d404071ccdee708b74af5
ogunsolu007 <ogunsolu30@gmail.com>
2022-04-27 15:47:24 +0100

    finished cart

--- src/App.js
+++ src/App.js
@@ -6,14 +6,34 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+// reducer  function is used to update store
+// reducer function accept two arg state and action
+// state is the initial state
+// action is what happened/ what is updated
+// reduucer function return the updated state or old state
+
+import { createStore } from "redux";
+import { reducer } from "./reducer";
+import { Provider } from "react-redux";
+// initial store
+
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+// REDUCER
+
+const store = createStore(reducer, initialStore);
+
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer/>
+    </Provider>
   );
 }
 
